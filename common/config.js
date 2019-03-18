3bd0902da3e41a7b327786f15f72dd39fe104f90
pjqdyd <1909025079@qq.com>
2019-03-18 16:51:18 +0800

    --主页布局基本完成--

--- common/config.js
+++ common/config.js
@@ -1,4 +1,5 @@
-const serverUrl = ' https://www.easy-mock.com/mock/5c8dd97d6fe7c7611499c703/test';
+// const serverUrl = ' https://www.easy-mock.com/mock/5c8dd97d6fe7c7611499c703/test';
+const serverUrl = 'https://www.fastmock.site/mock/06691bb14f8b8fc529239d05e90a0be9/slife';
 
 export default {
 	serverUrl
