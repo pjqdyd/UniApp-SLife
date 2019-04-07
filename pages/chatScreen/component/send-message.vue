c50875cb2f81e4dbe5cee538c248a6b708f0298d
pjqdyd <1909025079@qq.com>
2019-04-07 17:31:31 +0800

    --引入聊天界面组件--

---
+++ pages/chatScreen/component/send-message.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="send-message">
+		<view class="input-wrapper"><input type="text" cursor-spacing ="0" adjust-position="true"></view>
+		<view class="iconfont send">&#xe634; 发送</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'send-message',
+		props: {
+			data: String,//聊天内容
+			time: String,//提取名称的一个字
+		}
+	}
+</script>
+
+<style lang="scss" scoped="true">
+	.send-message{
+		box-sizing: border-box;
+		padding: 0 15upx;
+		height: 100upx;
+		width: 100%;
+		background:#ea5455;
+		line-height: 100upx;
+		display: flex;
+		flex-wrap: nowrap;
+		.input-wrapper{
+			flex:1;
+			padding: 15upx;
+			input{
+				box-sizing: border-box;
+				padding: 0 15upx;
+				width: 100%;
+				height: 100%;
+				border-radius: 10upx;
+				background: #fff;
+			}
+		}
+		.send{
+			color: #fff;
+		}
+	}
+</style>
